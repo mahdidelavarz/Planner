3d28661b78d9b6b6c53c087dc71ff4153ede17fc
mehdi delavar <delavar@petco.it>
2024-11-27 08:13:37 +0330

    project upgraded to typeScript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import routes from "./routes/routes";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
