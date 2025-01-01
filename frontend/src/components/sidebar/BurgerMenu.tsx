af32dffdcbc318920a8481741b2c82bd5fce523f
mdelavar903@gmail.com <mdelavar903@gmail.com>
2025-01-01 20:50:58 +0330

    burger menu updated!

--- frontend/src/components/sidebar/BurgerMenu.tsx
+++ frontend/src/components/sidebar/BurgerMenu.tsx
@@ -2,12 +2,12 @@ import { BurgerMenuProps } from "../../types/PropsTypes";
 function BurgerMenu({ isOpen }: BurgerMenuProps) {
   return (
     <div
-      className={`w-full h-full fixed top-0 right-0  mt-[58px] leanerLiftMenu ${
+      className={`w-full h-full fixed top-0 right-0  mt-[58px]   ${
         isOpen ? "flex" : "hidden"
       }`}
     >
-      <div className="w-[250px] bg-primary-950 shadow-md shadow-black"></div>
-      <div className="calc-width bg-primary-900 opacity-50"></div>
+      <div className="w-[250px] bg-primary-950 shadow-md shadow-black leanerLiftMenu"></div>
+      <div className="calc-width bg-primary-900 opacity-50 slowShow"></div>
     </div>
   );
 }
